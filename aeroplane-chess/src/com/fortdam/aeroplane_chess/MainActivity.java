54903c7d86502fd0b462735dbdc6f62e34f85364
fortdam <fortdam@hotmail.com>
2012-12-20 18:18:38 +0800

    add test button

--- aeroplane-chess/src/com/fortdam/aeroplane_chess/MainActivity.java
+++ aeroplane-chess/src/com/fortdam/aeroplane_chess/MainActivity.java
@@ -1,11 +1,23 @@
 package com.fortdam.aeroplane_chess;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	public void startTest(View view){
+		Context context = getApplicationContext();
+		String text = "Hello toast!";
+		int duration = Toast.LENGTH_SHORT;
 
+		Toast toast = Toast.makeText(context, text, duration);
+		toast.show();
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
